765f7f30516a7d7039587bbd41d894c230e1c585
Cheikh-Laay Dieye <email>
2025-12-25 21:25:33 +0100

    Sauvegarde du formulaire avant l'ajout

---
+++ partials/footer.php
@@ -0,0 +1,4 @@
+<!-- Footer -->
+    <footer class="mt-4">
+    <p class="text-center">Créé avec <span class="text-danger fs-4">&#9829;</span> par les DWWM22.</p>
+    </footer>
---
+++ partials/nav.php
@@ -0,0 +1,8 @@
+
+
+    <nav class="text-center shadow bg-white py-3">
+        <a href="/index.php" class="text-decoration-none mrpopcorn-brand text-dark fs-4">
+            <i class="fa-solid fa-film text-primary"></i>
+            Mr Popcorn
+        </a>
+    </nav>
